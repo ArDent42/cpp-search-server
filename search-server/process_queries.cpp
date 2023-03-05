3530be151e8814d9bd2d61082d18d9026fe779fb
ArDent42 <sergey.sushentsov@yandex.ru>
2023-03-05 16:42:23 +0300

    sprint 9

--- search-server/process_queries.cpp
+++ search-server/process_queries.cpp
@@ -11,7 +11,7 @@ std::vector<std::vector<Document>> ProcessQueries(
 		const std::vector<std::string> &queries) {
 	std::vector<std::vector<Document>> results(queries.size());
 	std::transform(std::execution::par, queries.begin(), queries.end(),
-			results.begin(), [search_server](const std::string &query) {
+			results.begin(), [&search_server](const std::string &query) {
 				return search_server.FindTopDocuments(query);
 			});
 	return results;
